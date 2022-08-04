146fe08f3ee1715b1e250ba5aeaed7a2c66dd403
Alex <xalexgs34@gmail.com>
2022-08-04 12:56:34 -0700

    Add mouseover effect to grid

--- javascript.js
+++ javascript.js
@@ -2,10 +2,15 @@ const body = document.body;
 const grid = document.createElement('div');
 
 grid.setAttribute('id', 'grid');
-
 body.appendChild(grid);
 
+// Create a 16x16 grid of squares 
 for (let i = 0; i < 256; i++){
     let square = document.createElement('div');
     grid.appendChild(square);
 }
+
+const squares = document.querySelectorAll('#grid > div');
+
+console.log(squares)
+squares.forEach((square) => {square.addEventListener('mouseover', () => {square.setAttribute('style', 'background-color: blue')})})
